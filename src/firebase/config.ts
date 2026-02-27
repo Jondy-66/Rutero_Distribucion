458f703b166229592a5efa869eb2258c4fdca2c9
Farmacia El Descuento <farmaciaeldescuentouio@gmail.com>
2026-02-27 22:36:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "rutero-fed",
+  "appId": "1:938904325205:web:95a0d28de4e2d913d118dc",
+  "apiKey": "AIzaSyBf28yfROnTCqwgLpXY-GJqIhwC7zIbQMo",
+  "authDomain": "rutero-fed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "938904325205"
+};
